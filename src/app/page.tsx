315831b7486ebc1a63cba23859af8479bcb224e9
muntasir010 <naeemtasir03@gmail.com>
2026-01-03 21:17:28 +0600

    initialized project

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-10 text-red-600 border-b-4">
+      <h1 className="text-xl text-red-500 ">Tailwind v4 is Working 🚀</h1>
+      <h1 className="text-7xl text-green-500 ">Tailwind v4 is not Working 🚀</h1>
+    </div>
+  );
+}
